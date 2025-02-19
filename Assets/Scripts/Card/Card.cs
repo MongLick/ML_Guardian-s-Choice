61824474899984c0dbdd2b8ad3e2b0b9c43c1e82
MongLick <qkjm4836@gmail.com>
2025-02-19 20:35:01 +0900

    feat : Card

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -8,6 +8,7 @@ public class Card : MonoBehaviour
 	[SerializeField] Image frontImage;
 	[SerializeField] Image backImage;
 	[SerializeField] RectTransform rectTransform;
+	[SerializeField] RectTransform maskRectTransform;
 
 	[Header("Specs")]
 	[SerializeField] float rotationSpeed;
@@ -16,6 +17,7 @@ public class Card : MonoBehaviour
 	{
 		backImage.gameObject.SetActive(true);
 		frontImage.gameObject.SetActive(false);
+		maskRectTransform.sizeDelta = new Vector2(0, maskRectTransform.sizeDelta.y);
 	}
 
 	public void FlipCard()
@@ -25,33 +27,40 @@ public class Card : MonoBehaviour
 
 	public void CardChoice()
 	{
-		Manager.Card.CardImage.gameObject.SetActive(false);
+		Manager.Card.CardParent.gameObject.SetActive(false);
 	}
 
 	private IEnumerator RotateCard()
 	{
-		yield return new WaitUntil(() => Manager.Card.CardImage.gameObject.activeSelf == true);
+		yield return new WaitUntil(() => Manager.Card.CardParent.gameObject.activeSelf == true);
 
-		float targetRotation = 180f;
-		float currentRotation = 0f;
+		float targetRotation = 0f;
+		float currentRotation = -180f;
+		rectTransform.rotation = Quaternion.Euler(0f, -180f, 0f);
 
 		while (currentRotation < targetRotation)
 		{
 			currentRotation += rotationSpeed * Time.deltaTime;
-			rectTransform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+			rectTransform.rotation = Quaternion.Euler(0f, currentRotation, 0f);
 
-			if (currentRotation > 90f)
+			float visibleRatio = Mathf.Clamp01((currentRotation + 180f) / 180f);
+			float maskWidth = Mathf.Lerp(0, 0, visibleRatio);
+			maskRectTransform.sizeDelta = new Vector2(maskWidth, maskRectTransform.sizeDelta.y);
+
+			if (currentRotation < -90f)
 			{
-				frontImage.gameObject.SetActive(true);
-				backImage.gameObject.SetActive(false);
+				frontImage.gameObject.SetActive(false);
+				backImage.gameObject.SetActive(true);
 			}
 			else
 			{
-				backImage.gameObject.SetActive(true);
-				frontImage.gameObject.SetActive(false);
+				backImage.gameObject.SetActive(false);
+				frontImage.gameObject.SetActive(true);
 			}
 
 			yield return null;
 		}
+
+		rectTransform.rotation = Quaternion.Euler(0f, 0f, 0f);
 	}
 }
--- Assets/Scripts/Manager/CardManager.cs
+++ Assets/Scripts/Manager/CardManager.cs
@@ -1,26 +1,72 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardManager : Singleton<CardManager>
 {
+	[SerializeField] List<GameObject> cardPrefabs = new List<GameObject>();
+	[SerializeField] List<GameObject> spawnedCards = new List<GameObject>();
+	[SerializeField] HashSet<int> selectedIndices = new HashSet<int>();
+
 	[Header("Components")]
-	[SerializeField] Card[] cards;
-	[SerializeField] GameObject cardImage;
-	public GameObject CardImage { get { return cardImage; } set { cardImage = value; } }
+	[SerializeField] Transform cardParent;
+	public Transform CardParent { get { return cardParent; } }
+
+	[Header("Vector")]
+	[SerializeField] Vector2[] anchorPositions;
 
 	private void Start()
 	{
-		cards = FindObjectsOfType<Card>();
-		cardImage = GameObject.FindGameObjectWithTag("CardImage");
-		cardImage.gameObject.SetActive(false);
+		cardParent = GameObject.FindGameObjectWithTag("CardImage").transform;
+		cardParent.gameObject.SetActive(false);
+	}
+
+	private void SpawnRandomCards()
+	{
+		foreach (GameObject card in spawnedCards)
+		{
+			Destroy(card);
+		}
+
+		spawnedCards.Clear();
+		selectedIndices.Clear();
+
+		while (selectedIndices.Count < 3)
+		{
+			int randomIndex = Random.Range(0, cardPrefabs.Count);
+			selectedIndices.Add(randomIndex);
+		}
+
+		int i = 0;
+
+		foreach (int index in selectedIndices)
+		{
+			GameObject cardObj = Instantiate(cardPrefabs[index], cardParent);
+			RectTransform cardTransform = cardObj.GetComponent<RectTransform>();
+
+			cardTransform.sizeDelta = new Vector2(300, 450);
+			cardTransform.anchorMin = anchorPositions[i];
+			cardTransform.anchorMax = anchorPositions[i];
+			cardTransform.anchoredPosition = Vector2.zero;
+			cardTransform.rotation = Quaternion.identity;
+
+			spawnedCards.Add(cardObj);
+			i++;
+		}
 	}
 
 	public void FlipAllCards()
 	{
-		cardImage.gameObject.SetActive(true);
+		cardParent.gameObject.SetActive(true);
 
-		foreach (Card card in cards)
+		SpawnRandomCards();
+
+		foreach (GameObject cardObj in spawnedCards)
 		{
-			card.FlipCard();
+			Card card = cardObj.GetComponent<Card>();
+			if (card != null)
+			{
+				card.FlipCard();
+			}
 		}
 	}
 }
